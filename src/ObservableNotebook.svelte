f1ee0723de560ce555445ef87dea0be0a65f1521
nitaku <matteo.abrate@gmail.com>
2021-02-22 16:51:23 +0100

    observableHQ component

---
+++ src/ObservableNotebook.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { onMount } from 'svelte'
+
+	import {Runtime, Inspector} from "@observablehq/runtime"
+	
+	export let notebook
+	export let variable_name
+	
+	let root_group
+	
+	// const dispatch = createEventDispatcher()
+	onMount(async function() {
+		new Runtime().module(notebook, (name) => {
+			if (name == variable_name) {
+				let inspector = new Inspector(root_group)
+				//dispatch('ready', { inspector })
+				return inspector
+			}
+		})
+	})
+</script>
+
+<g bind:this={root_group}></g>
