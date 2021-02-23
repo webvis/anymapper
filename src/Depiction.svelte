53132f01b0b3533e831f194b0f5f331e9e667038
nitaku <matteo.abrate@gmail.com>
2021-02-23 15:33:47 +0100

    infobox depiction component

---
+++ src/Depiction.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { Media } from '@smui/card'
+
+    export let src = null
+    export let size = 'cover'
+    export let aspectRatio = "16x9"
+</script>
+
+<style>
+</style>
+
+<Media style="background: url({src}), linear-gradient(180deg, rgba(245,245,245,0) 0%, rgba(245,245,245,1) 100%); background-size: {size}; background-position-x: center; background-position-y: center; background-repeat: no-repeat;" aspectRatio={aspectRatio}>
+</Media>
