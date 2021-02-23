90822012d3cd8a3a07839e6f79650b25e985662f
nitaku <matteo.abrate@gmail.com>
2021-02-23 15:45:51 +0100

    depiction API cleanup

--- src/Depiction.svelte
+++ src/Depiction.svelte
@@ -4,10 +4,12 @@
     export let src = null
     export let size = 'cover'
     export let aspectRatio = "16x9"
+    export let positionX = 'center'
+    export let positionY = 'center'
 </script>
 
 <style>
 </style>
 
-<Media style="background: url({src}), linear-gradient(180deg, rgba(245,245,245,0) 0%, rgba(245,245,245,1) 100%); background-size: {size}; background-position-x: center; background-position-y: center; background-repeat: no-repeat;" aspectRatio={aspectRatio}>
+<Media style="background: url({src}), linear-gradient(180deg, rgba(245,245,245,0) 0%, rgba(245,245,245,1) 100%); background-size: {size}; background-position-x: {positionX}; background-position-y: {positionY}; background-repeat: no-repeat;" aspectRatio={aspectRatio}>
 </Media>
