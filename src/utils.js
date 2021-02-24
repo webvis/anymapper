ba52ba269d5ebd1cfcfc9f7ff39d3c258ee0b26b
nitaku <matteo.abrate@gmail.com>
2021-02-24 13:13:05 +0100

    default selector in make_selectable

--- src/utils.js
+++ src/utils.js
@@ -3,7 +3,10 @@ import * as d3 from 'd3'
 import { select } from './stores.js'
 
 export function make_selectable(root, selector, accessor) {
-    if (accessor === undefined) // default accessor
+    if (!selector) // default selector
+        selector = '.selectable'
+        
+    if (!accessor) // default accessor
         accessor = (d) => d3.select(d).attr('id')
 
     d3.select(root).selectAll(selector).on('click', function () {
