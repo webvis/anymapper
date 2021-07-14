fbcb7d6e225acf99331e3250e83be0f8eb0e012f
nitaku <matteo.abrate@gmail.com>
2021-07-14 14:56:29 +0200

    tracing paper support

--- src/Layer.svelte
+++ src/Layer.svelte
@@ -1,18 +1,22 @@
 <script>
 	import { fade } from 'svelte/transition'
 
-	import { layers } from './stores.js'
+	import { layers, current_layer } from './stores.js'
+	import TracingPaper from './TracingPaper.svelte'
 
 	export let name
 	export let type = 'overlay'
 
 	$: visible = $layers && $layers.has(name) && $layers.get(name).visible
+	$: current = $current_layer && $current_layer.name == name
+	$: first = $layers && $layers.keys().next().value == name
 </script>
 
 <g class="layer" data:name="{name}" data:type="{type}">
 	{#if visible}
-	    <g transition:fade>
-			<slot></slot>
-		</g>
+		{#if type == 'floor' && current && !first}
+			<TracingPaper/>
+		{/if}
+		<slot></slot>
 	{/if}
 </g>
---
+++ src/TracingPaper.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { semantic_zoom } from './stores.js'
+</script>
+
+<style>
+</style>
+
+<rect transform="scale({$semantic_zoom})"
+    fill="white"
+    fill-opacity="0.7"
+    x="-50000"
+    y="-50000"
+    width="100000"
+    height="100000"
+/>
