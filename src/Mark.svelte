22b4e9f4c1848c6670bf36afa581781694a9834e
nitaku <matteo.abrate@gmail.com>
2022-02-02 13:33:57 +0100

    better circle mark

--- src/Mark.svelte
+++ src/Mark.svelte
@@ -15,7 +15,7 @@
 
 <g class="mark">
     {#if shape == 'circle'}
-        <circle r="14" fill="#777" stroke="#777" stroke-width="1" cy="1"/>
+        <circle r="14" cy="2" opacity="0.35" stroke="black" stroke-width="2"/>
         <circle r="14" fill={bg} stroke={fg} stroke-width="2"/>
     {:else if shape == 'square'}
         <rect width="28" height="28" x="-14" y="-12" rx="4" ry="4" opacity="0.35" stroke="black" stroke-width="2"/>
@@ -25,6 +25,6 @@
     {#if text}
         <text class="label" fill={fg} text-anchor="middle" dy=".35em">{text}</text>
     {:else if icon}
-        <text class="material-icons" fill={fg} transform="scale(0.9)" text-anchor="middle" dy=".5em">{icon}</text>
+        <text class="material-icons" fill={fg} transform="scale(0.8)" text-anchor="middle" dy=".5em">{icon}</text>
     {/if}
 </g>
