3c5d351ed2091263bded640e5775b346b9a6b79b
nitaku <matteo.abrate@gmail.com>
2022-02-02 13:18:11 +0100

    zoom-based mark

--- src/Mark.svelte
+++ src/Mark.svelte
@@ -8,23 +8,23 @@
 
 <style>
 .label {
-    font-size: 80px;
+    font-size: 18px;
     font-weight: bold;
 }
 </style>
 
 <g class="mark">
     {#if shape == 'circle'}
-        <circle r="62" fill="#777" stroke="#777" stroke-width="5" cy="5"/>
-        <circle r="62" fill={bg} stroke={fg} stroke-width="7.5"/>
+        <circle r="14" fill="#777" stroke="#777" stroke-width="1" cy="1"/>
+        <circle r="14" fill={bg} stroke={fg} stroke-width="2"/>
     {:else if shape == 'square'}
-        <rect width="124" height="124" x="-62" y="-57" rx="16" ry="16" fill="#777" stroke="#777" stroke-width="5"/>
-        <rect width="124" height="124" x="-62" y="-62" rx="16" ry="16" fill={bg} stroke={fg} stroke-width="7.5"/>
+        <rect width="28" height="28" x="-14" y="-12" rx="4" ry="4" opacity="0.35" stroke="black" stroke-width="2"/>
+        <rect width="28" height="28" x="-14" y="-14" rx="4" ry="4" fill={bg} stroke={fg} stroke-width="2"/>
     {/if}
 
     {#if text}
-        <text class="label" fill={fg} text-anchor="middle" dy=".4em">{text}</text>
+        <text class="label" fill={fg} text-anchor="middle" dy=".35em">{text}</text>
     {:else if icon}
-        <text class="material-icons" fill={fg} transform="scale(3.5)" text-anchor="middle" dy=".5em">{icon}</text>
+        <text class="material-icons" fill={fg} transform="scale(0.9)" text-anchor="middle" dy=".5em">{icon}</text>
     {/if}
 </g>
