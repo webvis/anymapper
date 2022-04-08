4f5a4ddab7ec8e083483c375f2fff647b692f64d
nitaku <matteo.abrate@gmail.com>
2022-04-08 16:17:03 +0200

    shadow offset independent from zoom

--- src/graphics/Marker.svelte
+++ src/graphics/Marker.svelte
@@ -14,6 +14,7 @@
     $: icons = icon === null ? [] : icon.split(',')
     $: width = 8 - icon_spacing + Math.max(1, icons.length) * (20+icon_spacing)
     $: actual_outline_width = outline_width / scale
+    $: shadow_offset = 2 / scale
 
     const PIN_D = 'M -0.64019711,-0.2956675 C -1.2209976,-0.8293218 -5.9552623,-9.1635097 -9.1320615,-15.244729 -14.703276,-25.90949 -17.594023,-33.501024 -17.590039,-37.456633 c 0.0069,-6.462469 3.71629,-12.576105 9.4258042,-15.533539 2.7082627,-1.402842 5.0884766,-1.988303 8.11060396,-1.994976 3.09596704,-0.0072 5.46376434,0.567925 8.21767004,1.99472 5.7102168,2.958489 9.4194808,9.071246 9.4259928,15.533795 0.0034,3.94629 -2.904594,11.591053 -8.4440082,22.193833 C 5.9655518,-9.1751947 1.2246711,-0.8331741 0.64000901,-0.2956675 0.44014188,-0.1119163 0.19786774,0 0,0 -0.19782487,0 -0.44026137,-0.1119722 -0.64019711,-0.2956675 Z'
 </script>
@@ -29,11 +30,11 @@
     <!-- shadow -->
     {#if shadow}
         {#if shape == 'circle'}
-            <circle r="14" cy="2" opacity="0.35" stroke="black" stroke-width={actual_outline_width}/>
+            <circle r="14" cy={shadow_offset} opacity="0.35" stroke="black" stroke-width={actual_outline_width}/>
         {:else if shape == 'square'}
-            <rect width={width} height="28" x={-width/2} y="-12" rx="4" ry="4" opacity="0.35" stroke="black" stroke-width={actual_outline_width}/>
+            <rect width={width} height="28" x={-width/2} y={-14+shadow_offset} rx="4" ry="4" opacity="0.35" stroke="black" stroke-width={actual_outline_width}/>
         {:else if shape == 'pin'}
-            <path opacity="0.35" stroke="black" stroke-width={actual_outline_width} transform="translate(0,2)" d={PIN_D}/>
+            <path opacity="0.35" stroke="black" stroke-width={actual_outline_width} transform="translate(0,{shadow_offset})" d={PIN_D}/>
         {/if}
     {/if}
 
