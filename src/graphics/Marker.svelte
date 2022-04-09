85f808c1beef4b0e244f1a493b3b52526cab2ca1
nitaku <matteo.abrate@gmail.com>
2022-04-09 13:39:56 +0200

    better icon spacing

--- src/graphics/Marker.svelte
+++ src/graphics/Marker.svelte
@@ -8,12 +8,13 @@
     export let text = null
     export let shape = 'circle'
     export let shadow = false
-    export let icon_spacing = 0
+    export let icon_spacing
     export let scale = 1
 
     $: icons = icon === null ? [] : icon.split(',')
     $: width = 8 - icon_spacing + Math.max(1, icons.length) * (20+icon_spacing)
     $: actual_outline_width = outline_width / scale
+    $: actual_icon_spacing = icon_spacing ? icon_spacing : 0
     $: shadow_offset = 2 / scale
 
     const PIN_D = 'M -0.64019711,-0.2956675 C -1.2209976,-0.8293218 -5.9552623,-9.1635097 -9.1320615,-15.244729 -14.703276,-25.90949 -17.594023,-33.501024 -17.590039,-37.456633 c 0.0069,-6.462469 3.71629,-12.576105 9.4258042,-15.533539 2.7082627,-1.402842 5.0884766,-1.988303 8.11060396,-1.994976 3.09596704,-0.0072 5.46376434,0.567925 8.21767004,1.99472 5.7102168,2.958489 9.4194808,9.071246 9.4259928,15.533795 0.0034,3.94629 -2.904594,11.591053 -8.4440082,22.193833 C 5.9655518,-9.1751947 1.2246711,-0.8331741 0.64000901,-0.2956675 0.44014188,-0.1119163 0.19786774,0 0,0 -0.19782487,0 -0.44026137,-0.1119722 -0.64019711,-0.2956675 Z'
@@ -49,7 +50,7 @@
     {#if text}
         <text class="label" fill={fg_color} text-anchor="middle" dy=".35em" y={shape == 'pin' ? -36 : 0}>{text}</text>
     {:else if icon}
-        <text class="material-icons{icon_set ? '-'+icon_set : ''}" fill={fg_color} transform="scale(0.8)" text-anchor="middle" dy=".5em" dx="{icon_spacing/2}px" y={shape == 'pin' ? -47 : 0} style="letter-spacing: {icon_spacing}px;">
+        <text class="material-icons{icon_set ? '-'+icon_set : ''}" fill={fg_color} transform="scale(0.8)" text-anchor="middle" dy=".5em" dx="{actual_icon_spacing/2}px" y={shape == 'pin' ? -47 : 0} style="letter-spacing: {actual_icon_spacing}px;">
             {#each icons as icon}
                 <tspan>{icon}</tspan>
             {/each}
