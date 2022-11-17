9befab056277fade15535d04580ec6ddfbb29cd3
nitaku <matteo.abrate@gmail.com>
2022-11-17 11:53:56 +0100

    components source tree reorganized + splitting of box header and close button

---
+++ src/boxes/content/CloseButton.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { clearSelection } from '../../stores.ts'
+
+    import { Actions, ActionIcons } from '@smui/card'
+    import IconButton from '@smui/icon-button'
+
+    export let iconSet = 'material-symbols-outlined'
+    export let closeIcon = 'close'
+</script>
+
+<style>
+</style>
+
+<div style="position: absolute; top: 2px; right: -4px;">
+    <Actions>
+        <ActionIcons>
+            <IconButton class={iconSet} on:click={clearSelection} style="color: var(--anymapper-primary-fg-color);" title="Close">{closeIcon}</IconButton>
+        </ActionIcons>
+    </Actions>
+</div>
---
+++ src/boxes/content/Header.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { Content } from '@smui/card'
+
+    export let title = 'Title'
+    export let subtitle = ''
+</script>
+
+<style>
+    h2, h3 {
+        margin: 0;
+        font-weight: normal;
+    }
+</style>
+
+<Content style="background: var(--anymapper-primary-bg-color); color: var(--anymapper-primary-fg-color);">
+    <h2 class="mdc-typography--headline6">{title}</h2>
+    <h3 class="mdc-typography--subtitle2">{subtitle}</h3>
+</Content>
